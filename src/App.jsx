91e74c45ba03463b080503f0f15cf0fabfd9d0bb
João Gabriel <joao.gabriel.fazio@gmail.com>
2026-04-18 02:25:44 -0300

    feat: componente Filtros com estado ativo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import "./App.css";
 import ResumoCard from "./components/ResumoCard";
+import { useState } from "react";
+import Filtros from "./components/Filtros";
 
 function App() {
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState("todos");
+
   return (
-    <div className="cards-container">
-      <ResumoCard titulo={"Saldo Atual"} icone={"💰"} valor={12450} />
-      <ResumoCard titulo="Total de Entradas" icone={"📈"} valor={562450} />
-      <ResumoCard titulo="Total de Saídas" icone={"📉"} valor={12450} />
-      
+    <div>
+      <div className="cards-container">
+        <ResumoCard titulo={"Saldo Atual"} icone={"💰"} valor={12450} />
+        <ResumoCard titulo="Total de Entradas" icone={"📈"} valor={562450} />
+        <ResumoCard titulo="Total de Saídas" icone={"📉"} valor={12450} />
+      </div>
+
+      <div className="filtros">
+        <Filtros
+          categoriaSelecionada={categoriaSelecionada}
+          onCategoriaChange={setCategoriaSelecionada}
+        />
+      </div>
     </div>
   );
 }
