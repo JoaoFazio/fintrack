de5cae972df26b96c5cb4e834092973173a6332f
João Gabriel <joao.gabriel.fazio@gmail.com>
2026-04-19 03:37:21 -0300

    feat: ListaTransacoes e ItemTransacao com filtro funcional

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,28 @@ import "./App.css";
 import ResumoCard from "./components/ResumoCard";
 import { useState } from "react";
 import Filtros from "./components/Filtros";
+import ListaTransacoes from "./components/ListaTransacoes/ListaTransacoes";
 
 function App() {
-  const [categoriaSelecionada, setCategoriaSelecionada] = useState("todos");
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState("Todos");
+  const [transacoes, setTransacoes] = useState([
+    {
+      id: "1",
+      descricao: "iFood",
+      valor: 45.9,
+      tipo: "saida",
+      categoria: "Alimentação",
+      data: "2026-04-16",
+    },
+    {
+      id: "2",
+      descricao: "Bolsa LabMulti",
+      valor: 1200,
+      tipo: "entrada",
+      categoria: "Outros",
+      data: "2025-04-01",
+    },
+  ]);
 
   return (
     <div>
@@ -18,6 +37,13 @@ function App() {
         <Filtros
           categoriaSelecionada={categoriaSelecionada}
           onCategoriaChange={setCategoriaSelecionada}
+        />
+      </div>
+
+      <div>
+        <ListaTransacoes
+          transacoes={transacoes}
+          categoriaSelecionada={categoriaSelecionada}
         />
       </div>
     </div>
